e252a1c116fc198dd9dd6331c7438793a49dcb86
Tuomas Hatakka <tuomas.hatakka@gmail.com>
2024-01-08 02:33:42 +0200

    Update trewzxtttt

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 function typeText (element) {
   let interval
   const text    = element.textContent
-  const letters = text.split('')
+  const letters  = text.split('')
 
   const createLetterNode = (letter) => {
     const node = document.createElement('span')
@@ -14,12 +14,13 @@ function typeText (element) {
     if (!letters.length)
       return clearInterval(interval);
 
-    const letter = letters.shift()
-    const node = createLetterNode(letter)
+    const letter  = letters.shift()
+    const node    = createLetterNode(letter)
+
     element.appendChild(node)
   }
 
-
+  element.textContent = ''
   interval = setInterval(typeNextLetter, 100)
 }
 
